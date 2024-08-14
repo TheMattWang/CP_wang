c74f312bdf7edc5d4f60f96f4349f53599836bd2
TheMattWang <mat.wang25@gmail.com>
2024-08-14 11:37:30 -0500

    creating strings

---
+++ begin/creating_strings.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+
+using ll = long long;
+using ld = long double;
+using namespace std;
+#define endl "\n";
+#define ff first
+#define ss second
+
+ll mod = 1e9 + 7;
+ll n,x;
+using vi = vector<int>;
+ll dp[1000001];
+void solve(string arg1, int index){
+    for(int i =0; i < arg1.length(); i++) {
+        
+    }
+}
+
+int main() {
+    string insert;
+    cin >> insert;
+    solve(insert,0);
+    return 0;
+}
